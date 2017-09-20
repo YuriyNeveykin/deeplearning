7a84e007bea378104bb4670722fa2cf079aade0d
YuriyNeveykin <nev.yu@outlook.com>
2017-09-21 00:26:20 +0500

    Add files via upload
    comment2

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import cv2, os
 import numpy as np
 import matplotlib.image as mpimg
-
+"""
+Comment
+"""
 
 IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
 INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
